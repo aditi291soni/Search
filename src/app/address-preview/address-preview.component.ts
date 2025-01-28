39903a0989a5f32a359797cd6be6cc735a738dec
Shivam Shrivastava <shivam.shrivastava1006@gmail.com>
2025-01-28 17:29:10 +0530

    order page completed

--- src/app/address-preview/address-preview.component.ts
+++ src/app/address-preview/address-preview.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-address-preview',
-  imports: [],
-  templateUrl: './address-preview.component.html',
-  styleUrl: './address-preview.component.css'
+   selector: 'app-address-preview',
+   standalone: true,
+   imports: [CommonModule, ButtonModule],
+   templateUrl: './address-preview.component.html',
+   styleUrls: ['./address-preview.component.css']
 })
 export class AddressPreviewComponent {
+   pickup = {
+      name: 'Aditi Mp',
+      phone: '73648236423',
+      address: '6, Hamidia Rd, Ghora Nakkas, Peer Gate Area, Bhopal, Madhya Pradesh 462001, India'
+   };
 
+   drop = {
+      name: 'Aditi',
+      phone: '362364723',
+      address: 'Minal Residency, Bhopal, Madhya Pradesh, India'
+   };
 }
