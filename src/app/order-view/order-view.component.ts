5da8a36e52e8f32ce9eafd4ffafc40826e05c7b2
Shivam Shrivastava <shivam.shrivastava1006@gmail.com>
2025-01-30 14:00:02 +0530

    30 jan

--- src/app/order-view/order-view.component.ts
+++ src/app/order-view/order-view.component.ts
@@ -1,185 +1,186 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TimelineModule } from 'primeng/timeline';
 import { ApiService } from '../services/api.service';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
    selector: 'app-order-view',
    standalone: true,
-   imports: [ButtonModule, TimelineModule],
+   imports: [ButtonModule, TimelineModule, CommonModule],
    templateUrl: './order-view.component.html',
    styleUrls: ['./order-view.component.css'],
 })
 export class OrderViewComponent {
-   events: any[];
-   order: any=0;
-   loading: boolean=false;
+   events: any[] = [];
+   isMobile = window.innerWidth < 768;
+   order: any = {};
+   loading: boolean = false;
    invoice: any;
    invoiceId: any;
    businessDetails: any;
    deliveryId: any;
    delivery_name: any;
    timeslot: any;
-   orderstatus: any;
+   orderstatus: any[] = [];
+   filteredOrderStatus: any[] = [];
+   timelineshow: boolean = false;
 
- constructor(private apiService: ApiService, private router: Router, private fb: FormBuilder,private route: ActivatedRoute) {
-   this.businessDetails = this.apiService.getLocalValueInJSON(localStorage.getItem('bussinessDetails'));
-   this.route.params.subscribe((params) => {
+   constructor(
+      private apiService: ApiService,
+      private router: Router,
+      private fb: FormBuilder,
+      private route: ActivatedRoute
+   ) {
+      this.businessDetails = this.apiService.getLocalValueInJSON(localStorage.getItem('bussinessDetails'));
 
-      this.invoiceId = params['invoice_id'];
-   });
-      this.events = [
-         {
-            status: 'Placed',
-            date: '2025-01-01',
-            description: 'Your order has been successfully placed.',
-            icon: 'pi pi-check-circle',
-            color: 'green',
-            lineColor: 'green', // Separator line color
-         },
-         {
-            status: 'Processing',
-            date: '2025-01-02',
-            description: 'Your order is being processed.',
-            icon: 'pi pi-check-circle',
-            color: 'green',
-            lineColor: 'green',
-         },
-         {
-            status: 'Shipped',
-            date: '2025-01-03',
-            description: 'Your order has been shipped.',
-            icon: 'pi pi-check-circle',
-            color: 'green',
-            lineColor: 'green',
-         },
-         {
-            status: 'Delivered',
-            date: '2025-01-05',
-            description: 'Your order has been delivered.',
-            icon: 'pi pi-circle-off', // Default (not checked)
-            color: 'gray',
-            lineColor: 'gray', // Gray separator for pending status
-         },
-      ];
-   }
-
-   ngOnInit(): void {
-      this.getInvoice()
-      this.getOrderStatus()
-     
-   }
-   getOrderDelivery(id:any): void {
-      let payload: any = {};
-      payload.super_admin_id = environment.superAdminId;
-      payload.order_delivery_details_id = id;
-      this.apiService.getOrderDeliveryDetail(payload).subscribe({
-         next: (response) => {
-            if (response.status === true) {
-               this.order = response.data || [];
-               this.getTimeSlot(this.order?.time_slot);
-               this.getDelivery(this.order?.delivery_type_id);
-            } else {
-               console.error('Error fetching vehicle types:', response.message);
-            }
-         },
-         error: (err) => {
-            console.error('Error fetching vehicle types:', err);
-         },
-         complete: () => {
-            this.loading = false;
-         },
+      this.route.params.subscribe((params) => {
+         this.invoiceId = params['invoice_id'];
       });
    }
 
+   ngOnInit(): void {
+      this.getInvoice();
+   }
+
+   @HostListener('window:resize', ['$event'])
+   onResize(event: Event) {
+      this.isMobile = (event.target as Window).innerWidth < 768;
+   }
+
+   getInvoice(): void {
+      let payload = {
+         super_admin_id: environment.superAdminId,
+         invoice_id: this.invoiceId,
+         invoice_type: 'order',
+      };
+
+      this.apiService.getInvoice(payload).subscribe({
+         next: (response) => {
+            if (response.status === true) {
+               this.invoice = response.data || {};
+               this.deliveryId = this.invoice.delivery_id;
+               this.getOrderDelivery(this.invoice.delivery_id);
+               this.getOrderStatus();
+            } else {
+               console.error('Error fetching invoice:', response.message);
+            }
+         },
+         error: (err) => console.error('Error fetching invoice:', err),
+         complete: () => (this.loading = false),
+      });
+   }
+
+   getOrderDelivery(id: any): void {
+      let payload = {
+         super_admin_id: environment.superAdminId,
+         order_delivery_details_id: id,
+      };
+
+      this.apiService.getOrderDeliveryDetail(payload).subscribe({
+         next: (response) => {
+            if (response.status === true) {
+               this.order = response.data || {};
+               this.getTimeSlot(this.order?.time_slot);
+               this.getDelivery(this.order?.delivery_type_id);
+            } else {
+               console.error('Error fetching order delivery:', response.message);
+            }
+         },
+         error: (err) => console.error('Error fetching order delivery:', err),
+         complete: () => (this.loading = false),
+      });
+   }
+
+   filterOrderStatus(): void {
+      if (this.order?.order_status_id === 33) {
+         this.filteredOrderStatus = this.orderstatus.filter(status => ![34, 26, 25].includes(status.id));
+      } else if (this.order?.order_status_id === 34) {
+         this.filteredOrderStatus = this.orderstatus.filter(status => ![33, 26, 25].includes(status.id));
+      } else if (this.order?.order_status_id === 32) {
+         this.filteredOrderStatus = this.orderstatus.filter(status => ![34, 26, 25].includes(status.id));
+      }
+   }
 
    getOrderStatus(): void {
-      let payload: any = {};
-      payload.super_admin_id = environment.superAdminId;
-      // payload.order_delivery_details_id = this.deliveryId;
+      let payload = {
+         super_admin_id: environment.superAdminId,
+      };
+
       this.apiService.getOrderStatus(payload).subscribe({
          next: (response) => {
             if (response.status === true) {
                this.orderstatus = response.data || [];
-           
+               this.updateTimeline(); // ✅ Update timeline dynamically
             } else {
-               console.error('Error fetching vehicle types:', response.message);
+               console.error('Error fetching order status:', response.message);
             }
          },
-         error: (err) => {
-            console.error('Error fetching vehicle types:', err);
-         },
-         complete: () => {
-            this.loading = false;
-         },
+         error: (err) => console.error('Error fetching order status:', err),
+         complete: () => (this.loading = false),
       });
    }
-   getDelivery(id:any): void {
-      let payload: any = {};
-      payload.super_admin_id = environment.superAdminId;
-      payload.delivery_type_id = id;
+
+   showTimeline(): void {
+      this.timelineshow = !this.timelineshow;
+   }
+
+   getDelivery(id: any): void {
+      let payload = {
+         super_admin_id: environment.superAdminId,
+         delivery_type_id: id,
+      };
+
       this.apiService.getDelivery(payload).subscribe({
          next: (response) => {
             if (response.status === true) {
-               this.delivery_name = response.data.delivery_name|| [];
+               this.delivery_name = response.data.delivery_name || [];
             } else {
-               console.error('Error fetching vehicle types:', response.message);
+               console.error('Error fetching delivery:', response.message);
             }
          },
-         error: (err) => {
-            console.error('Error fetching vehicle types:', err);
-         },
-         complete: () => {
-            this.loading = false;
-         },
+         error: (err) => console.error('Error fetching delivery:', err),
+         complete: () => (this.loading = false),
       });
    }
 
+   getTimeSlot(id: any): void {
+      let payload = {
+         super_admin_id: environment.superAdminId,
+         time_slot_id: id,
+      };
 
-   getTimeSlot(id:any): void {
-      let payload: any = {};
-      payload.super_admin_id = environment.superAdminId;
-      payload.time_slot_id = id;
       this.apiService.getTimeSlots(payload).subscribe({
          next: (response) => {
             if (response.status === true) {
                this.timeslot = response.data || [];
             } else {
-               console.error('Error fetching vehicle types:', response.message);
+               console.error('Error fetching time slot:', response.message);
             }
          },
-         error: (err) => {
-            console.error('Error fetching vehicle types:', err);
-         },
-         complete: () => {
-            this.loading = false;
-         },
+         error: (err) => console.error('Error fetching time slot:', err),
+         complete: () => (this.loading = false),
       });
    }
-    getInvoice(): void {
-      let payload: any = {};
-      payload.super_admin_id = environment.superAdminId;
-      payload.invoice_id = this.invoiceId;
-      payload.invoice_type ='order';
-         this.apiService.getInvoice(payload).subscribe({
-            next: (response) => {
-               if (response.status === true) {
-                  this.invoice = response.data || [];
-                  this.deliveryId = this.invoice.delivery_id;
-                  this.getOrderDelivery(response.data.delivery_id);
-               } else {
-                  console.error('Error fetching vehicle types:', response.message);
-               }
-            },
-            error: (err) => {
-               console.error('Error fetching vehicle types:', err);
-            },
-            complete: () => {
-               this.loading = false;
-            },
-         });
+
+   updateTimeline(): void {
+      if (!this.order || !this.order.order_status_id || this.orderstatus.length === 0) {
+         return;
       }
+
+      // Find the current order's index in the orderstatus array
+      const currentIndex = this.orderstatus.findIndex(status => status.id === this.order.order_status_id);
+
+      this.events = this.orderstatus.map((status, index) => ({
+         status: status.name_for_user,
+         date: '', // Add date field if needed
+         description: `Order is ${status.name_for_user}`,
+         icon: index <= currentIndex ? 'pi pi-check-circle' : 'pi pi-circle-off',
+         color: index <= currentIndex ? 'green' : 'gray',
+         lineColor: index <= currentIndex ? 'green' : 'gray',
+      }));
+   }
 }
