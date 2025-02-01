a2d05d98329683a958da4cb2e23ef399149ecd8d
Shivam Shrivastava <shivam.shrivastava1006@gmail.com>
2025-02-01 10:54:21 +0530

    build files added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -7,7 +7,8 @@ import { routes } from './app.routes';
 import Noir from '../themes/mypreset';
 import { authInterceptor } from './auth.interceptor'; // Import the interceptor
 import { MessageService } from 'primeng/api'; // Import MessageService
-import { ToastModule } from 'primeng/toast';  // Import ToastModule
+import { ToastModule } from 'primeng/toast';
+import { provideServiceWorker } from '@angular/service-worker';  // Import ToastModule
 
 export const appConfig: ApplicationConfig = {
    providers: [
@@ -19,7 +20,13 @@ export const appConfig: ApplicationConfig = {
       }),
       provideRouter(routes),
       provideHttpClient(withInterceptors([authInterceptor])), // Register the interceptor here
-      MessageService, provideAnimationsAsync(),  // Provide MessageService globally
+      MessageService, provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),  // Provide MessageService globally
       // ToastModule,     // Ensure ToastModule is available globally
    ],
 };
